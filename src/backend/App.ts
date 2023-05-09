5aa248f9758bd97ddab851f214f137f99dd16d8f
JulenJR <julenjr@hotmail.com>
2023-05-09 10:39:30 +0200

    added PUT /player/:id

--- src/backend/App.ts
+++ src/backend/App.ts
@@ -1,5 +1,5 @@
 import { Server } from "./Server";
-import { Request, Response } from "express";
+import express,{ Request, Response } from "express";
 import jwt from 'jsonwebtoken';
 
 export class App {
@@ -13,4 +13,31 @@ export class App {
 	}
 }
 
+const app = express();
+const secretKey = 'my_secret_key';
 
+interface Player {
+	name : string;
+}
+
+let players : Player[];
+
+app.use(express.json());
+
+app.post('/player', (req : Request, res : Response) => {
+	const { name } = req.body;
+
+	if (players.find((player) => player.name === name)){
+		return res.status(400).json({ error: 'Player name already exist' });
+	}
+
+	const player : Player = { name };
+	players.push(player);
+
+	const jwtoken = jwt.sign({ name }, secretKey);
+	res.json({ player, jwtoken });
+});
+
+app.listen(8000, () => {
+	console.log('Server is listening on port 8000');
+  });
