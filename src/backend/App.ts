5c59d6da94f946dabf2432f1d41b7456208c8fe8
JulenJR <julenjr@hotmail.com>
2023-05-09 13:13:05 +0200

    added PUT /players/{id}: change id player

--- src/backend/App.ts
+++ src/backend/App.ts
@@ -17,26 +17,55 @@ const app = express();
 const secretKey = 'my_secret_key';
 
 interface Player {
-	name : string;
+	id : string;
 }
 
 let players : Player[];
 
 app.use(express.json());
 
+//POST /player: create a new player if the name doesnt exist jet
 app.post('/player', (req : Request, res : Response) => {
-	const { name } = req.body;
+	
+	let { id } = req.body;
 
-	if (players.find((player) => player.name === name)){
+	if (!id) id = 'anonymous'
+
+	if (players.find((player) => player.id === id && player.id != 'anonymous')){
 		return res.status(400).json({ error: 'Player name already exist' });
 	}
 
-	const player : Player = { name };
+	const player : Player = { id };
 	players.push(player);
 
-	const jwtoken = jwt.sign({ name }, secretKey);
+	const jwtoken = jwt.sign({ id }, secretKey);
 	res.json({ player, jwtoken });
 });
+
+//PUT /players/{id}: change the name of the player for another that deosnt exist jet
+app.put('/player/:id', (req : Request, res : Response) => {
+
+	const playerId = req.params.id;
+	const { newId } = req.body;
+
+	const playerIndex = players.findIndex((player) => player.id === playerId);
+  if (playerIndex === -1) {
+    return res.status(404).json({ error: 'Player not found' });
+  }
+
+  if (players.find((player) => player.id === newId)){
+	return res.status(400).json({ error: 'This name already' })
+  }
+
+  players[playerIndex].id = newId;
+
+  const jwtoken = jwt.sign({ id: playerId, newId }, secretKey);
+  res.json({ player: players[playerIndex], jwtoken });
+
+});
+
+
+
 
 app.listen(8000, () => {
 	console.log('Server is listening on port 8000');
