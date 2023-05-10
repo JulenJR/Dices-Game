2509c2dee81e157cd4ae3e30ec861ed3287d7c15
JulenJR <julenjr@hotmail.com>
2023-05-10 12:58:32 +0200

    ongoing mongoose  tests

--- src/backend/App.ts
+++ src/backend/App.ts
@@ -1,84 +1,100 @@
-import express, { Request, Response } from "express";
-import { json } from "body-parser";
-import jwt from "jsonwebtoken";
 import { Server } from "./Server";
-import mongoose from 'mongoose';
-
-const secretKey = "my_secret_key";
+import express,{ Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import mongoose, { Schema, ConnectOptions } from 'mongoose';
 
 interface Player {
-  id: string;
-  password: string;
+	id : string;
+	password : string;
 }
 
-let players: Player[];
+const playerSchema = new Schema({
+	id: { type: String, required: true },
+	password: { type: String, required: true }
+});
+
+const PlayerModel = mongoose.model<Player>('Player', playerSchema);
+
+const app = express();
+const secretKey = 'my_secret_key';
+
+app.use(express.json());
+
+//POST /login: authenticate if the data provided by user are correct
+app.post('/login', async (req: Request, res : Response) => {
+	const { id, password } = req.body;
+
+	const user = await PlayerModel.findOne({ id });
+	if(!user || password !== user.password ){
+		return res.status(401).json({ error : 'The username or password are incorrect' });
+	}
+
+	const token = jwt.sign({id : user.id}, secretKey);
+
+	res.json({token});
+});
+
+//POST /player: create a new player if the name doesnt exist jet
+app.post('/player', async (req : Request, res : Response) => {
+	let { id, password } = req.body;
+
+	if (!id) id = 'anonymous'
+
+	if (await PlayerModel.exists({ id })) {
+		return res.status(400).json({ error: 'Player name already exist' });
+	}
+
+	const player : Player = { id, password };
+	await PlayerModel.create(player);
+
+	const jwtoken = jwt.sign({ id }, secretKey);
+	res.json({ player, jwtoken });
+});
+
+//PUT /players/{id}: change the name of the player for another that deosnt exist jet
+app.put('/player/:id', async (req : Request, res : Response) => {
+	const playerId = req.params.id;
+	const { newId } = req.body;
+
+	const player = await PlayerModel.findOne({ id: playerId });
+	if (!player) {
+		return res.status(404).json({ error: 'Player not found' });
+	}
+
+	if (await PlayerModel.exists({ id: newId })) {
+		return res.status(400).json({ error: 'This name already exists' })
+	}
+
+	player.id = newId;
+	await player.save();
+
+	const jwtoken = jwt.sign({ id: playerId, newId }, secretKey);
+	res.json({ player, jwtoken });
+});
 
 export class App {
-  server?: Server;
+	server?: Server;
 
-  async start(): Promise<void> {
-    const port = process.env.PORT ?? "8000";
-    this.server = new Server(port);
+	async start(): Promise<void> {
+		const port = process.env.PORT ?? "8000";
+		this.server = new Server(port);
 
-    const gameApp = this.server.express;
-
-    gameApp.use(json());
-
-    //POST /login: authenticate if the data provided by user are correct
-    gameApp.post("/login", (req: Request, res: Response) => {
-      const { id, password } = req.body;
-
-      const user = players.find((user) => user.id === id);
-      if (!user || password !== user.password) {
-        return res
-          .status(401)
-          .json({ error: "The username or password are incorrect" });
-      }
-
-      const token = jwt.sign({ id: user.id }, secretKey);
-
-      res.json({ token });
-    });
-
-    //POST /player: create a new player if the name doesnt exist jet
-    gameApp.post("/player", (req: Request, res: Response) => {
-      let { id, password } = req.body;
-
-      if (!id) id = "anonymous";
-
-      if (players.find((player) => player.id === id && player.id != "anonymous")) {
-        return res
-          .status(400)
-          .json({ error: "Player name already exists" });
-      }
-
-      const player: Player = { id, password };
-      players.push(player);
-
-      const jwtoken = jwt.sign({ id }, secretKey);
-      res.json({ player, jwtoken });
-    });
-
-    //PUT /players/{id}: change the name of the player for another that doesn't exist yet
-    gameApp.put("/player/:id", (req: Request, res: Response) => {
-      const playerId = req.params.id;
-      const { newId } = req.body;
-
-      const playerIndex = players.findIndex((player) => player.id === playerId);
-      if (playerIndex === -1) {
-        return res.status(404).json({ error: "Player not found" });
-      }
-
-      if (players.find((player) => player.id === newId)) {
-        return res.status(400).json({ error: "This name already exists" });
-      }
-
-      players[playerIndex].id = newId;
-
-      const jwtoken = jwt.sign({ id: playerId, newId }, secretKey);
-      res.json({ player: players[playerIndex], jwtoken });
-    });
-
-    await this.server.listen();
-  }
+		await this.server.listen();
+	}
 }
+
+try {
+	void new App().start();
+} catch (e) {
+	process.exit(1);
+}
+
+process.on("uncaughtException", () => {
+	process.exit(1);
+});
+
+const options: ConnectOptions = {
+};
+
+// Connect to the database
+mongoose.connect('mongodb+srv://dbuser:dbUserPss@atlascluster.ccvayit.mongodb.net/', options);
